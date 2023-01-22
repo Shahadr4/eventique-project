8352eb47a8f6bfc8724e5f3ef2935fcb45dbbc37
shahad rahman <shahadr47@gmail.com>
2023-01-22 23:00:01 +0530

    update nav bar

--- lib/screen/home/homescreen/home.dart
+++ lib/screen/home/homescreen/home.dart
@@ -1,4 +1,6 @@
+import 'package:evtq/const/logo.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -7,6 +9,36 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("home"),);
+    return const Catogory();
+  }
+}
+
+class Catogory extends StatelessWidget {
+  const Catogory({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromARGB(255, 247, 247, 247),
+      child: ListView(
+        
+        children: [
+          appLogo,
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: CupertinoSearchTextField(
+              style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+            ),
+          )
+
+
+          
+
+
+        ],
+      ),
+    );
   }
 }
